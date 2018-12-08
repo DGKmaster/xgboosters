abc13cd016dbf5231858c772488bc60ac2a6b0fa
Timur <timursurhaev@gmail.com>
2018-12-08 15:26:20 +0300

    + install_readme, MANIFEST.in, setup.py для версии 1.1.0

---
+++ code/SmartHouse-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "SmartHouse"
+NAME = "smarthouse"
+DESCRIPTION = ""
+AUTHOR = "sgb"
+AUTHOR_EMAIL = ""
+URL = ""
+VERSION = "1.1.0"
+ 
+with open('releasenotes.txt', 'r') as rn:
+    releasenotes = rn.read()  
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=releasenotes,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    url=URL,
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    zip_safe=False,
+)
+
