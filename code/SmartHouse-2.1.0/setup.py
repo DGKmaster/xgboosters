f3ba81efc0dd6b986efa0798ac48dadd5f8e5b5b
Timur <timursurhaev@gmail.com>
2018-12-09 14:24:03 +0300

    editing .travis.yml

---
+++ code/SmartHouse-2.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "SmartHouse"
+NAME = "smarthouse"
+DESCRIPTION = ""
+AUTHOR = "sgb"
+AUTHOR_EMAIL = ""
+URL = ""
+VERSION = "2.1.0"
+ 
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    #long_description=releasenotes,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    url=URL,
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    zip_safe=False,
+)
+
